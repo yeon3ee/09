8f75a0c3e453ddfcbb3330a6713217730214a1ac
yeon3ee <2310538@sookmyung.ac.kr>
2023-11-02 23:08:02 +0900

    03_2

--- mainn.c
+++ mainn.c
@@ -8,18 +8,19 @@
 int main(int argc, char *argv[]) {
 	
 	int i;
-	int grade[N_STUDENT];
-	int score[N_STUDENT];
+	int a[N_STUDENT]={1,2,3,4,5};
+	int b[N_STUDENT]={1,2,3,4,6};
+	int flag =0;
+	for(i=0;i<N_STUDENT;i++){
 	
-	for(i=0;i<N_STUDENT;i++)
-		grade[i]=rand()%100+1;
-		
-	for(i=0;i<N_STUDENT;i++)
-		score[i]=grade[i];
-		
-	for(i=0;i<N_STUDENT;i++)
-		printf("score[%d]=%d(grade:%i)\n,",i,score[i],grade[i]);
-	
+		if(a[i]!=b[i])
+		{
+			printf("array a and b are not the same");
+			flag=1;
+		}	
+	}
+	if(flag==0)
+		printf("array a and b are the same\n");
 	return 0;
 	
 }
